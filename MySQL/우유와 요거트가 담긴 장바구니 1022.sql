4b34920c2b06ad96035c7dc7180bc7207f0024b3
SeraWi <wsr2357@naver.com>
2021-10-22 11:41:06 +0900

    [1022] Programmers mySQL 문제 풀이

--- MySQL/우유와 요거트가 담긴 장바구니 1022.sql
+++ MySQL/우유와 요거트가 담긴 장바구니 1022.sql
@@ -1,6 +1,8 @@
 
 
 -- 우유와 요거트가 담긴 장바구니
+-- 데이터 분석 팀에서는 우유(Milk)와 요거트(Yogurt)를 동시에 구입한 장바구니가 있는지 알아보려 합니다. 
+-- 우유와 요거트를 동시에 구입한 장바구니의 아이디를 조회하는 SQL 문을 작성해주세요. 이때 결과는 장바구니의 아이디 순으로 나와야 합니다.
 
 SELECT DISTINCT(C1.CART_ID)
 FROM (SELECT * FROM CART_PRODUCTS WHERE NAME='MILK') AS C1 INNER JOIN
---
+++ MySQL/이름이 없는 동물의 아이디 1022.sql
@@ -0,0 +1,54 @@
+-- 이름이 없는 동물의 아이디
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NULL
+ORDER BY ANIMAL_ID;
+
+-- NULL을 조회하기
+-- '컬럼 이름 ' IS NULL
+-- NULL이 아닌 행 조회하기
+-- '컬럼 이름' IS NOT NULL
+-- 컬럼이름 = NULL 으로 조회하면 안된다!(주의!!)
+
+-- 이름이 있는 동물의 아이디
+SELECT ANIMAL_ID
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL
+ORDER BY ANIMAL_ID ASC;
+
+-- 양 게시판에 동물 정보를 게시하려 합니다. 
+-- 동물의 생물 종, 이름, 성별 및 중성화 여부를 아이디 순으로 조회하는 SQL문을 작성해주세요.  --> 아이디 순 정렬
+-- 이때 프로그래밍을 모르는 사람들은 NULL이라는 기호를 모르기 때문에, 이름이 없는 동물의 이름은 "No name"으로 표시해 주세요.
+
+SELECT ANIMAL_TYPE, NVL(NAME,'No name') AS NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID;
+
+-- NULL 을 치환하는 함수 NVL(컬럼이름, 바꿀 내용)
+
+
+-- 천재지변으로 인해 일부 데이터가 유실되었습니다. 
+-- 입양을 간 기록은 있는데, 보호소에 들어온 기록이 없는 동물의 ID와 이름을 ID 순으로 조회하는 SQL문을 작성해주세요.
+
+SELECT OUTS.ANIMAL_ID, OUTS.NAME
+FROM ANIMAL_OUTS OUTS LEFT OUTER JOIN ANIMAL_INS INS 
+ON OUTS.ANIMAL_ID = INS.ANIMAL_ID
+WHERE INS.ANIMAL_ID IS NULL
+ORDER BY OUTS.ANIMAL_ID;
+
+
+-- 관리자의 실수로 일부 동물의 입양일이 잘못 입력되었습니다. 
+-- 보호 시작일보다 입양일이 더 빠른 동물의 아이디와 이름을 조회하는 SQL문을 작성해주세요.
+--  이때 결과는 보호 시작일이 빠른 순으로 조회해야합니다.
+
+SELECT i.animal_id, i.name
+from animal_ins i, animal_outs o
+where i.animal_id = o.animal_id 
+and i.datetime > o.datetime
+order by i.datetime;
+
+SELECT INS.ANIMAL_ID, INS.NAME
+FROM ANIMAL_INS INS INNER JOIN ANIMAL_OUTS OUTS
+ON INS.ANIMAL_ID = OUTS.ANIMAL_ID
+WHERE INS.DATETIME > OUTS.DATETIME
+ORDER BY INS.DATETIME;
