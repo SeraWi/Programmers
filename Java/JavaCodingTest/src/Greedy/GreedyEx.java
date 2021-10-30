2277ab7ee2ca2760127a84f7bff9106e84d3a848
SeraWi <wsr2357@naver.com>
2021-10-30 16:15:53 +0900

    Create GreedyEx.java

---
+++ Java/JavaCodingTest/src/Greedy/GreedyEx.java
@@ -0,0 +1,11 @@
+package Greedy;
+
+public class GreedyEx {
+	
+	
+	public static void main(String[] args) {
+		int[] people = new int[5]; //현재 {0,0,0,0,0}
+		
+		System.out.println(people[3]);
+	}
+}
