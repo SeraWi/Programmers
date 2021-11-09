49409a036a568286b1399add4f5ac16d48889381
SeraWi <wsr2357@naver.com>
2021-11-09 16:37:32 +0900

    Update MySolution.java

--- Java/JavaCodingTest/src/Kakao2019/MySolution.java
+++ Java/JavaCodingTest/src/Kakao2019/MySolution.java
@@ -12,6 +12,8 @@ import java.util.Map.Entry;
 // 2019 KakaoBlindRecruitment
 // 실패율: 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
 // 통과!!!
+// 전체 스테이지의 개수 N, 게임을 이용하는 사용자가 현재 멈춰있는 스테이지의 번호가 담긴 배열 stages가 매개변수로 주어질 때, 
+// 실패율이 높은 스테이지부터 내림차순으로 스테이지의 번호가 담겨있는 배열을 return 하도록 solution 함수를 완성하라.
 public class MySolution {
 	public static ArrayList solution(int N, int[] stages) {
 		// n: 전체 스테이지 갯수
