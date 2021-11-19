4c575ff53ad77773b17e815b4811071641321cb7
SeraWi <wsr2357@naver.com>
2021-11-19 12:49:03 +0900

    Programmers: 카카오 2021 Blind Recruitment Coding Test - 신규 아이디 추천 (통과)

--- Java/JavaCodingTest/src/Kakao2021Recruit/MySolution.java
+++ Java/JavaCodingTest/src/Kakao2021Recruit/MySolution.java
@@ -1,7 +1,5 @@
 package Kakao2021Recruit;
 
-import java.util.regex.Pattern;
-
 //2021 카카오 Blind Recruitment
 // 신규 아이디 추천
 public class MySolution {
@@ -21,7 +19,7 @@ public class MySolution {
 		// 2단계 알파벳 소문자, 숫자, 빼기 밑줄, 마침표 외에 글자 제외하기
 		// \\d : 숫자
 		answer = answer.replaceAll("[^a-z\\d\\-_.]*", "");
-		
+
 		// 3단계 마침표(.)가 2번 이상 연속된 부분을 하나의 마침표(.)로 치환합니다.
 		while(answer.contains("..")) {
 			answer = answer.replace("..", ".");
@@ -31,36 +29,36 @@ public class MySolution {
 			// 처음 인덱스
 			answer = answer.replaceFirst(".", "");
 		}
-		
+
 		if(answer.endsWith(".")) {
 			// 마지막 인덱스 .이면 앞에 까지 잘라내기
 			answer = answer.substring(0, answer.length()-1);
-				
+
 		}
 		//5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
 		if(answer.length()==0) {
 			answer ="a";
 		}
 		//6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다.
-	    if(answer.length() >=16) {
-	    	answer = answer.substring(0,15);
-	    }
+		if(answer.length() >=16) {
+			answer = answer.substring(0,15);
+		}
 		// 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
 		if(answer.endsWith(".")) {
 			// 마지막 인덱스
 			answer = answer.substring(0, answer.length()-1);
 		}
-		
+
 		//7단계 7단계 new_id의 길이가 2자 이하라면, 
 		//new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
-		
+
 		if(answer.length() <=2) {
 			while(answer.length() <3) {
 				answer += answer.substring(answer.length()-1,answer.length());
 			}
 		}
-		
-		
+
+
 		return answer;
 	} // solution 함수 끝
 
