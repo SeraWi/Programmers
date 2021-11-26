4779982cb5277dad51a1560050a981b7fb126f47
SeraWi <wsr2357@naver.com>
2021-11-26 17:45:09 +0900

    2021 카카오 blind recruitment

---
+++ Java/JavaCodingTest/src/Kakao2021Recruit/NewCourse.java
@@ -0,0 +1,20 @@
+package Kakao2021Recruit;
+
+
+//각 손님들이 주문한 단품메뉴들이 문자열 형식으로 담긴 배열 orders, 
+//"스카피"가 추가하고 싶어하는 코스요리를 구성하는 단품메뉴들의 갯수가 담긴 배열 course가 매개변수로 주어질 때, "스카피"가 새로 
+//추가하게 될 코스요리의 메뉴 구성을 문자열 형태로 배열에 담아 return 하도록 solution 함수를 완성해 주세요.
+public class NewCourse {
+    public String[] solution(String[] orders, int[] course) {
+    	//코스 요리의 메뉴 구성은 손님 2명이 그렇게 시킨적이 있을 경우에만 추가 가능
+    	//정답은 각 코스요리 메뉴의 구성을 문자열 형식으로 배열에 담아 사
+    	// 전 순으로 오름차순 정렬해서 return 해주세요. -> answer 오름차순 시키기!
+    	
+    	
+    	
+    	
+    	
+        String[] answer = {};
+        return answer;
+    }
+}
