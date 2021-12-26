2670581757cb2246ed9088629e4241ad0ae6aea2
SeraWi <wsr2357@naver.com>
2021-12-26 16:34:25 +0900

    카카오 2019 recuitmet - 후보키 경우의 수 구하기(비트연산사용하기)

--- Java/JavaCodingTest/src/Kakao2019Recruit/CandidateKey.java
+++ Java/JavaCodingTest/src/Kakao2019Recruit/CandidateKey.java
@@ -1,13 +1,22 @@
 package Kakao2019Recruit;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CandidateKey {
 	// int = 4bytes = 32 bits 1byte = 8bit
 	
 	// n<<i의 의미 : n 숫자의 비트를 좌측으로 i만큼 이동시킨다.
-	
+	private static boolean possi(List<Integer> list, int now) {
+		for( int i=0; i<list.size(); i++) {
+			if((list.get(i) &now) == list.get(i)) {
+				return false;
+			}
+		}
+		return true;
+	}
 	public static int solution(String[][] relation) {
 		int answer = 0;
 		
@@ -15,12 +24,29 @@ public class CandidateKey {
 		int m = relation[0].length; //세로
 		
 		List<Integer> list =new ArrayList<Integer>();
+		// 세로 길이 만큼 비트연산으로 밀기 1000000
+		for(int i=1; i<(1<<m); i++) {
+			Set<String> s= new HashSet<String>();
+			
+			for(int j=0;j<n; j++) {
+				String now ="";
+				for( int k=0; k<m;k++) {
+					if((i & (1<<k))>0) {
+						now += relation[j][k];
+					}
+				}
+				s.add(now);
+			}
+			
+			if(s.size()== n && possi(list, i)) {
+				list.add(i);
+			}
+		}
 		
 		
 		
 		
-		
-		return answer;
+		return list.size();
 	}
 	public static void main(String[] args) {
 		String[][] relation = {
