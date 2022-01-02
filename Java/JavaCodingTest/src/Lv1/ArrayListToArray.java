10ab9ce934cf7e9d83ed519e8e6294c56c7fcc5f
SeraWi <wsr2357@naver.com>
2022-01-02 14:36:38 +0900

    Create ArrayListToArray.java

---
+++ Java/JavaCodingTest/src/Lv1/ArrayListToArray.java
@@ -0,0 +1,26 @@
+package Lv1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayListToArray {
+	public static void main(String[] args) {
+		String [] array = new String[3];
+		array[0] = "Test1";
+		array[1] = "test2";
+		array[2] ="test3";
+		// 배열 -> arrayList
+		// Arrays.asList(배열);
+		ArrayList<String> arrayList = new ArrayList<>(Arrays.asList(array));
+		System.out.println(arrayList);
+		
+		// arrayList ->배열
+		// arrayList.toArray()
+		String [] array2 = arrayList.toArray(new String[arrayList.size()])		;
+		System.out.println(array2[0]);
+		System.out.println(array2[1]);
+		System.out.println(array2[2]);
+		
+	}
+	
+}
