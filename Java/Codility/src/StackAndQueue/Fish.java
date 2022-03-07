ff38b844313e1f7bf43b12a87d01fee71693a097
SeraWi <wsr2357@naver.com>
2022-03-07 22:37:37 +0900

    Codility Coding Test - Stack And Queue

---
+++ Java/Codility/src/StackAndQueue/Fish.java
@@ -0,0 +1,14 @@
+package StackAndQueue;
+
+public class Fish {
+	/*
+	 * A 배열은 Fish의 크기를 나타내며, 큰 물고기가 마주치는 물고기를 잡아먹음.
+	 * B 배열은 방향을 나타내며, 1은 downstream,
+	 * 0은 upstream. 물고기의 속력을 일정하여 방향이 같으면 영원히 만나지 않는다.
+	 */
+	public int solution(int[] A, int[] B) {
+        // write your code in Java SE 8
+		
+		return 0;
+    }
+}
