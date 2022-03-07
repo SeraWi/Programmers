8c795acab238edf096bd811d9b49ecf9943a85e0
SeraWi <wsr2357@naver.com>
2022-03-07 22:37:17 +0900

    Codility Coding Test - Counting element

---
+++ Java/Codility/src/countingelement/MaxCounters.java
@@ -0,0 +1,32 @@
+package countingelement;
+
+public class MaxCounters {
+	public int[] solution(int N, int[] A) {
+		// write your code in Java SE 8
+		
+		int[] answer = new int[N];
+		int base =0;
+		int max =0;
+		
+		for( int i= 0; i<A.length; i++) {
+			if(A[i] >=1 && A[i] <= N) {
+				if(answer[A[i]-1] <base) {
+					answer[A[i] -1]= base;
+				}
+				
+				if(++answer[A[i]-1] > max) {
+					max = answer [A[i] -1];
+				}
+			}else if(A[i] == N+1) {
+				base = max;
+			}
+		}
+		
+		for( int  i = 0; i<answer.length; i++) {
+			if(answer[i] <base) {
+				answer[i] = base;
+			}
+		}
+		return answer;
+	}
+}
